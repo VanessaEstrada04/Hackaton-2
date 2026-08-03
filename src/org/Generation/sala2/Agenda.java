2cbac38e09abf80d515d9983758fd5020a81fc9c
Cristian Rodriguez <cristianharris14@gmail.com>
2026-08-03 14:22:04 -0600

    Metodo eliminar y modificar

--- src/org/Generation/sala2/Agenda.java
+++ src/org/Generation/sala2/Agenda.java
@@ -18,4 +18,37 @@ public class Agenda {
         this.capacidadMax = 10;
 
     }
+    //Metodo eliminarContacto
+    public void eliminarContacto(Contacto c) {
+        // Buscamos si existe en la lista comparando por nombre y apellido
+        Contacto contactoEncontrado = null;
+
+        for (Contacto contactoActual : contactos) {
+            if (contactoActual.getNombreCompleto().equalsIgnoreCase(c.getNombreCompleto())) {
+                contactoEncontrado = contactoActual;
+                break;
+            }
+        }
+        if (contactoEncontrado != null) {
+            contactos.remove(contactoEncontrado);
+            System.out.println("El contacto se ha eliminado exitosamente.");
+        } else {
+            System.out.println("No se pudo eliminar: El contacto no existe en la agenda.");
+        }
+    }
+    // 2. Método modificar telefono
+    public void modificarTelefono(String nombre, String apellido, String nuevoTelefono) {
+        String nombreBuscado = nombre + " " + apellido;
+
+        for (Contacto contactoActual : contactos) {
+            if (contactoActual.getNombreCompleto().equalsIgnoreCase(nombreBuscado)) {
+                contactoActual.setNumeroTelefono(nuevoTelefono);
+                System.out.println("Teléfono actualizado exitosamente.");
+                return;
+            }
+        }
+        System.out.println("No se pudo modificar: El contacto no existe en la agenda.");
+    }
 }
+
+
--- src/org/Generation/sala2/Contacto.java
+++ src/org/Generation/sala2/Contacto.java
@@ -1,7 +1,39 @@
 package org.Generation.sala2;
+import java.util.Scanner;
 
 public class Contacto {
     private String primerNombre;
     private String primerApellido;
     private int numeroTelefono;
+
+
+    public Contacto(String primerNombre, String primerApellido, int numeroTelefono) {
+        this.primerNombre = primerNombre;
+        this.primerApellido = primerApellido;
+        this.numeroTelefono = numeroTelefono;
+    }
+
+    public String getPrimerNombre() {
+        return primerNombre;
+    }
+
+    public void setPrimerNombre(String primerNombre) {
+        this.primerNombre = primerNombre;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+
+    public int getNumeroTelefono() {
+        return numeroTelefono;
+    }
+
+    public void setNumeroTelefono(int numeroTelefono) {
+        this.numeroTelefono = numeroTelefono;
+    }
 }
